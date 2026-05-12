f27575c91ebbe8a8ac6feee2048f39db88d5f379
Vuk Bojovic <vuki253@gmail.com>
2026-05-12 02:08:27 +0200

    Create config.py and setup directory structure
    Add initial configuration file and directory structure setup.

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# Ti kreiras fajlove lokalno:
+mkdir -p src/data src/embedding src/retrieval src/llm src/rag src/evaluation src/chatbot src/utils
+touch src/__init__.py
+touch src/config.py
+# itd...
+git add .
+git commit -m "Add Python modules"
+git push
